eb485e7477457e59bacdef3e67b74c5021d2ac84
rookieanvesh <132641928+rookieanvesh@users.noreply.github.com>
2024-05-14 22:30:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //use all js code as newer version
+//alert(3+3) we are using nodejs not browser
+
+console.log(3
+    +3)// code readability should be high
+
+let numer = 12123
+let name = "anvesh"
+let sachyajhoot = false
+let state;
+/*
+number => 2^53
+bigint
+string = " "
+boolean = true/false
+null, htis is actually a stand alone value
+undefined = variable is decalred but value is undefined 
+symbol = unique
+these are the primitves data types
+
+objects
+
+*/
+console.log(typeof "anv");
