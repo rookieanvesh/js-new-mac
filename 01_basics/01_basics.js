4dd1da582e8744b87ddf80a002a5344e46e16a2e
rookieanvesh <132641928+rookieanvesh@users.noreply.github.com>
2024-05-14 22:31:20 +0000

    basics

---
+++ 01_basics/01_basics.js
@@ -0,0 +1,15 @@
+const accountId = 234234
+let accountEmail = "sdsdfsf@gmail.com"
+var accountPassword = "3121hb34"
+accountCity = "jaipur"
+let acccountState;
+/*
+prefer not to use var because if issue in block and fucntional scope
+*/
+
+accountEmail = "132323@okbi"
+accountPassword = "238293892"
+accountCity = "bhopal"
+//accountId = 2 not possible
+console.log(accountId);
+console.table([accountEmail,acccountState,accountPassword,accountCity])
