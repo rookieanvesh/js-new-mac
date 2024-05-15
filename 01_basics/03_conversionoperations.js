0bd037faeb4b3a7633f10653d6c111cff6cf9228
rookieanvesh <132641928+rookieanvesh@users.noreply.github.com>
2024-05-15 22:01:17 +0000

    conversions

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,19 @@
+let score = "33abc"
+ console.log(typeof score);
+ console.log(typeof (score));
+
+ //agar koi operation hai jo sirf number ke sath karna hai oth we can do this
+ let valueInNumber = Number(score)
+ console.log(valueInNumber);
+ console.log(typeof valueInNumber);
+ //NaN is not a number
+
+ let truu = 1
+ let truuboolorna = Boolean(truu)
+ console.log(truuboolorna);
+
+ //1 true
+ // 0 flse
+  // " " false
+  // "anvesh" true
+  // similarly we can do for strings as well 
