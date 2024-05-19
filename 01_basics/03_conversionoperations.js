de5c7d07a25df04bd8f8414ede3d3f9f5616fca8
rookieanvesh <132641928+rookieanvesh@users.noreply.github.com>
2024-05-19 00:02:27 +0000

    data stuff

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -17,3 +17,15 @@ let score = "33abc"
   // " " false
   // "anvesh" true
   // similarly we can do for strings as well 
+
+  //**********************OPERATIONS */
+  // basic math operations +,-,/,%,*
+  let str1 = "anv"
+  let str2 = " " + "huh"
+  let str3 = str1 + str2
+  console.log(str3)
+  console.log("1"+2)//all will be converted to string
+  console.log(1+2+"3")//here the first 2 will be added first then then they will be added to a string
+  //never use this type of complicated code instead use brackets and stuff
+  console.log(+true);
+  console.log(+"");//aise tricky conversions maat kiya karo
---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,7 @@
+//always compare same data types
+// equality and comparison operators work differently 
+console.log(null==0);//false
+console.log(null>=0);//true
+//undefined is also similar ot this but it gives false values to all the operations
+// === is called a strict check,it not only checks the value but also the data types
+//we should mostly avoid these type of conversions
